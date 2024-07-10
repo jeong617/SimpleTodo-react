411bcd01006ef753ef9d2b928ca8679cb5405423
jeong617 <loodol99@naver.com>
2024-07-10 11:37:43 +0900

    project setting

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import * as React from 'react';
+import Button from '@mui/material/Button';
+
+
+function App() {
+  return <Button variant="contained">Hello world</Button>;
+}
+
+export default App;
