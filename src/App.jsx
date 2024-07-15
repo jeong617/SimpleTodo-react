7b48d445338b13642159f7cda2d1b11709d59b2a
jeong617 <loodol99@naver.com>
2024-07-15 11:40:30 +0900

    feat: add todo input

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,18 @@
 import * as React from 'react';
 import './App.css';
+import ToDoInputContainer from './containers/ToDoInputContainer'
 import Button from '@mui/material/Button';
 
 function App() {
+  const [todos, setTodos] = React.useState([]);
+  const addTodo = (todo) => {
+    setTodos([...todos, todo]);
+  }
+
   return (
     <div className="container">
       <h1>TODO LIST</h1>
-
+      <ToDoInputContainer addTodo={addTodo}/>
     </div>
   )
 }
---
+++ src/containers/ToDoInputContainer.jsx
@@ -0,0 +1,24 @@
+import React, {useState} from 'react';
+
+export default function ToDoInputContainer(props) {
+    const [inputBox, setInputBox] = useState('');
+    const handleChange = (e) => {
+        setInputBox(e.target.value);
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        if (inputBox.trim() !== '') {
+            props.addTodo(inputBox);
+            setInputBox('');
+        }
+    }
+    return (
+        <div className="todo-input">
+            <form onSubmit={handleSubmit}>
+                <input type="text" value={inputBox} onChange={handleChange} placeholder='할 일을 입력하세요!' />
+                <button type="submit">추가</button>
+            </form>
+        </div>
+    )
+}
